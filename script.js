dc0e835f527701d28189b79ff6ff30f29e6c0f1d
Wade <couplesuicide@proton.me>
2026-03-25 14:52:34 +0500

    portfolio

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const audio = document.getElementById('bg-music');
+    const toggleBtn = document.getElementById('audio-toggle');
+    const mutedIcon = document.getElementById('icon-muted');
+    const playingIcon = document.getElementById('icon-playing');
+
+    let isPlaying = false;
+
+    toggleBtn.addEventListener('click', () => {
+        if (isPlaying) {
+            audio.pause();
+            mutedIcon.classList.remove('hidden');
+            playingIcon.classList.add('hidden');
+        } else {
+            audio.play().catch(e => console.log("Нужен клик для старта музыки"));
+            mutedIcon.classList.add('hidden');
+            playingIcon.classList.remove('hidden');
+        }
+        isPlaying = !isPlaying;
+    });
+});
