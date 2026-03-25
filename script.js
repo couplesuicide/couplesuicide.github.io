6049d55995f32e3b1a31f61489d01cda1402f4c3
Wade <couplesuicide@proton.me>
2026-03-25 16:14:55 +0500

    fix

--- script.js
+++ script.js
@@ -3,18 +3,23 @@ document.addEventListener('DOMContentLoaded', () => {
     const toggleBtn = document.getElementById('player-toggle');
     const playIcon = document.getElementById('icon-play');
     const pauseIcon = document.getElementById('icon-pause');
+
     let isPlaying = false;
 
-    toggleBtn.addEventListener('click', () => {
-        if (isPlaying) {
-            audio.pause();
-            playIcon.classList.remove('hidden');
-            pauseIcon.classList.add('hidden');
-        } else {
-            audio.play().catch(() => console.log("Нужен клик"));
-            playIcon.classList.add('hidden');
-            pauseIcon.classList.remove('hidden');
-        }
-        isPlaying = !isPlaying;
-    });
+    if (toggleBtn && audio) {
+        toggleBtn.addEventListener('click', () => {
+            if (isPlaying) {
+                audio.pause();
+                playIcon.classList.remove('hidden');
+                pauseIcon.classList.add('hidden');
+            } else {
+                audio.play().catch(e => {
+                    console.log("Нужен клик по странице для старта");
+                });
+                playIcon.classList.add('hidden');
+                pauseIcon.classList.remove('hidden');
+            }
+            isPlaying = !isPlaying;
+        });
+    }
 });
